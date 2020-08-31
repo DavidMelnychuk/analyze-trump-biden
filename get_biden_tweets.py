6841816c190b1bdc7256507f13f34a187b55f3a2
David Melnychuk <davidm9779@gmail.com>
2020-08-30 22:53:43 -0700

    add .env file, update dir structure

--- get_biden_tweets.py
+++ get_biden_tweets.py
@@ -1,12 +1,17 @@
 import tweepy
 import csv
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # User credentials to access Twitter API
-# Put in .ENV File if making public on github
-ACCESS_TOKEN = '1021581972227776513-PqahU1pcZSeqAJHqsccSuUB7BystUi'
-ACCESS_SECRET = 'LJJok1yWAhUCI371MnwBmxtsDGSMrzi51aaON3X1GButI'
-CONSUMER_KEY = 'sGvMZG52HeaBqBywIT0Ttx4ZD'
-CONSUMER_SECRET = 'ULyYcj0q0TgS0eTIJpAjanWeqAGeLh9ZuLofQYes33klEMSGAn'
+# Put your twitter credentials in a .env File if making public
+
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_SECRET = os.getenv("ACCESS_SECRET")
+CONSUMER_KEY = os.getenv("CONSUMER_KEY")
+CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
 
 # Authentication
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -16,9 +21,10 @@ api = tweepy.API(auth)
 # 3200 is the maximum number of tweets available from a twitter user timeline
 num_tweets = 3200
 
-biden_tweets_csv = open('biden.csv', 'w', encoding="utf_8_sig", newline='')
+biden_tweets_csv = open('test.csv', 'w', encoding="utf_8_sig", newline='')
 csv_writer = csv.writer(biden_tweets_csv)
-csv_writer.writerow(['text', 'created_at', 'retweet_count', 'favorite_count', 'is_retweet', 'id_str'])
+csv_writer.writerow(['text', 'created_at', 'retweet_count',
+                     'favorite_count', 'is_retweet', 'id_str'])
 biden_tweets = []
 
 for tweet in tweepy.Cursor(api.user_timeline,
